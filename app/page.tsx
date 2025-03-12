2a332ad078b77794ef5468404abf44f8e8907657
Abhijith Kannan <abhijithkannan452@outlook.com>
2025-03-12 23:09:21 +0530

    udpate

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,12 @@
 import { SampleForm } from "@/components/home/sample-form";
 // import LoginPage from "@/app/login/page";
 import { sampleFormData } from "@/data/form-data";
-import Head from 'next/head'
 
 export default async function Home() {
   return (
     <div className="min-h-screen flex justify-center my-8">
-      <Head>
-        <title>Upskill 2025</title>
-      </Head>
       <SampleForm formData={sampleFormData} />
-
+{/* <LoginPage></LoginPage> */}
     </div>
   );
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -6,8 +6,8 @@ import Image from "next/image";
 import { SignupForm } from "../../components/ui/signup-form";
 
 export default function SignupPage() {
-    return (
-        <div className="grid min-h-screen w-full overflow-hidden lg:grid-cols-2">
+  return (
+    <div className="grid min-h-screen w-full overflow-hidden lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
@@ -24,17 +24,17 @@ export default function SignupPage() {
         </div>
       </div>
       <div className="relative hidden lg:flex items-center justify-center w-full h-screen bg-muted">
-  <div className="relative w-[60%] max-w-lg h-[80%]">
-    <Image
-      src={loginImage}
-      alt="Login Image"
-      layout="fill"
-      objectFit="contain" // Prevents stretching and cropping
-      className="dark:brightness-[0.2] dark:grayscale rounded-lg" // Optional: Add rounded corners
-    />
-  </div>
-</div>
+        <div className="relative w-[60%] max-w-lg h-[80%]">
+          <Image
+            src={loginImage}
+            alt="Login Image"
+            layout="fill"
+            objectFit="contain" // Prevents stretching and cropping
+            className="dark:brightness-[0.2] dark:grayscale rounded-lg" // Optional: Add rounded corners
+          />
+        </div>
+      </div>
 
     </div>
-    );
+  );
 }
