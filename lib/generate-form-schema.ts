ddc5772e592560544dc30c31869fb36ff1819af9
Abhijith Kannan <abhijithkannan452@outlook.com>
2025-03-12 23:04:01 +0530

    Update generate-form-schema.ts

--- lib/generate-form-schema.ts
+++ lib/generate-form-schema.ts
@@ -3,7 +3,6 @@ import { FormField } from "@/types/form-schema";
 
 export const generateFormSchema = (formFields: FormField[]) => {
     return z.object(
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         formFields.reduce((acc: Record<string, any>, field) => {
             let schema;
             switch (field.fieldType) {
