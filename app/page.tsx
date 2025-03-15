fb38f129497a65946805d8f564c9fdd5efcecee7
Abhijith Kannan <abhijithkannan452@outlook.com>
2025-03-16 01:29:55 +0530

    fixed build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 'use server'
 
-import { SampleForm } from "@/components/home/sample-form";
-// import LoginPage from "@/app/login/page";
-import { sampleFormData } from "@/data/form-data";
+// import { SampleForm } from "@/components/home/sample-form";
+// // import LoginPage from "@/app/login/page";
+// import { sampleFormData } from "@/data/form-data";
 
 export default async function Home() {
   return (
     <div className="min-h-screen flex justify-center my-8">
-      <SampleForm formData={sampleFormData} />
+      {/* <SampleForm formData={sampleFormData} /> */}
 {/* <LoginPage></LoginPage> */}
     </div>
   );
